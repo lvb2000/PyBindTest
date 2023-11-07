c57435b25a02d77eb97d9a1e3dcd289cc9e06aa8
Lukas <lukas@auterion.com>
2023-11-07 11:06:03 +0100

    pybind11 example

---
+++ test.py
@@ -0,0 +1,9 @@
+import build.module_name
+from build.module_name import *
+
+print(dir(build.module_name))
+print(some_fn_python_name(3,5))
+m = PySomeClass(5)
+print(m.multiply(5))
+print(m.multiply_list([1,2,3,4,5]))
+print(m.multiply_two(3,4))
